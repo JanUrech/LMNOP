681b1e690d78aeb60903ce7ef3a517cd7f31355e
JanUrech <janic.urech@stud.fhgr.ch>
2025-10-10 11:28:15 +0200

    Zusätzliche Prototypen Mitglieder hinzugefügt.

--- mockupData/mockupMembers.js
+++ mockupData/mockupMembers.js
@@ -3,41 +3,52 @@ const teammembers = [
     Name: "Pascal",
     role: "TeamLead",
     themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/protoBildStandard.svg",
-    fotoHover: "media/TeamMitglieder/protoBildHover.svg"
+    fotoStandard: "media/TeamMitglieder/PascalStandard.svg",
+    fotoHover: "media/TeamMitglieder/PascalFisheye.svg"
   },
 
   {
     Name: "Moritz",
     role: "TeamLead",
     themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/protoBildStandard.svg",
-    fotoHover: "media/TeamMitglieder/protoBildHover.svg"
+    fotoStandard: "media/TeamMitglieder/MoritzStandard.svg",
+    fotoHover: "media/TeamMitglieder/MoritzFisheye.svg"
   },
 
-    {
+  {
     Name: "Norina",
     role: "TeamLead",
     themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/protoBildStandard.svg",
-    fotoHover: "media/TeamMitglieder/protoBildHover.svg"
+    fotoStandard: "media/TeamMitglieder/NorinaStandard.svg",
+    fotoHover: "media/TeamMitglieder/NorinaFisheye.svg"
   },
 
-    {
+  {
     Name: "David",
     role: "TeamLead",
     themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/protoBildStandard.svg",
-    fotoHover: "media/TeamMitglieder/protoBildHover.svg"
+    fotoStandard: "media/TeamMitglieder/DavidStandard.svg",
+    fotoHover: "media/TeamMitglieder/DavidFisheye.svg"
   },
 
-    {
+  {
     Name: "Murielle",
-    role: "TeamLead",
+    role: "Social Media-Team",
     themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/protoBildStandard.svg",
-    fotoHover: "media/TeamMitglieder/protoBildHover.svg"
+    fotoStandard: "media/TeamMitglieder/MurielleStandard.svg",
+    fotoHover: "media/TeamMitglieder/Murielle Fisheye.svg"
   },
+
+  {
+    Name: "Janic",
+    role: "Webseite-Team",
+    themengebiet: "Männlichkeit",
+    fotoStandard: "media/TeamMitglieder/JanicStandard.svg",
+    fotoHover: "media/TeamMitglieder/JanicFisheye.svg"
+
+
+
+  }
 
 
 
