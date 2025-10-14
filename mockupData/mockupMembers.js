1612e34a0780eef0e8885fe390156948cdc6c98c
JanUrech <janic.urech@stud.fhgr.ch>
2025-10-14 13:08:17 +0200

    Päscu bild angepasst

--- mockupData/mockupMembers.js
+++ mockupData/mockupMembers.js
@@ -1,55 +1,57 @@
 const teammembers = [
   {
-    Name: "Pascal",
-    role: "TeamLead",
-    themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/PascalStandard.JPG",
-    fotoHover: "media/TeamMitglieder/PascalFisheye.JPG"
-  },
-
-  {
-    Name: "Moritz",
-    role: "TeamLead",
-    themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/MoritzStandard.JPG",
-    fotoHover: "media/TeamMitglieder/MoritzFisheye.JPG"
-  },
-
-  {
-    Name: "Norina",
-    role: "TeamLead",
-    themengebiet: "Gesundheitssystem",
-    fotoStandard: "media/TeamMitglieder/NorinaStandard.JPG",
-    fotoHover: "media/TeamMitglieder/NorinaFisheye.JPG"
-  },
-
-  {
     Name: "David",
-    role: "TeamLead",
+    role: "Lektorat",
     themengebiet: "Gesundheitssystem",
     fotoStandard: "media/TeamMitglieder/DavidStandard.JPG",
     fotoHover: "media/TeamMitglieder/DavidFisheye.JPG"
   },
 
   {
+    Name: "Janic",
+    role: "Webseite",
+    themengebiet: "Männlichkeit",
+    fotoStandard: "media/TeamMitglieder/JanicStandard.JPG",
+    fotoHover: "media/TeamMitglieder/JanicFisheye.JPG"
+  },
+
+  {
     Name: "Marie",
-    role: "Social Media-Team",
+    role: "Rechtsabteilung",
     themengebiet: "Gesundheitssystem",
     fotoStandard: "media/TeamMitglieder/MarieStandard.JPG",
     fotoHover: "media/TeamMitglieder/MarieFisheye.JPG"
   },
 
   {
-    Name: "Janic",
-    role: "Webseite-Team",
+    Name: "Moritz",
+    role: "Rechtsabteilung",
+    themengebiet: "Musik Szene",
+    fotoStandard: "media/TeamMitglieder/MoritzStandard.JPG",
+    fotoHover: "media/TeamMitglieder/MoritzFisheye.JPG"
+  },
+
+  {
+    Name: "Norina",
+    role: "Teamlead",
     themengebiet: "Männlichkeit",
-    fotoStandard: "media/TeamMitglieder/JanicStandard.JPG",
-    fotoHover: "media/TeamMitglieder/JanicFisheye.JPG"
+    fotoStandard: "media/TeamMitglieder/NorinaStandard.JPG",
+    fotoHover: "media/TeamMitglieder/NorinaFisheye.JPG"
+  },
 
+  {
+    Name: "Pascal",
+    role: "Teamlead",
+    themengebiet: "Gesundheitssystem",
+    fotoStandard: "media/TeamMitglieder/PascalStandard.JPG",
+    fotoHover: "media/TeamMitglieder/PascalFisheye.JPG"
+  },
 
-
+  { 
+    Name: "Giacumin",
+    role: "Webseite",
+    themengebiet: "Männlichkeit",
+    fotoStandard: "media/TeamMitglieder/GiacuminStandard.JPG",
+    fotoHover: "media/TeamMitglieder/GiacuminFisheye.JPG"
   }
-
-
-
 ];
